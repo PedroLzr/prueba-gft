f908946cb4c777e88d0842bd0f70c6053d443954
PedroLzr <cereus992@gmail.com>
2025-01-14 00:12:47 +0100

    Eliminar directorio target

--- project/src/main/java/com/pedrosanz/pruebatec/application/exception/PriceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.pedrosanz.pruebatec.application.exception;
-
-public class PriceNotFoundException extends RuntimeException {
-
-    public PriceNotFoundException(String message) {
-        super(message);
-    }
-
-}
